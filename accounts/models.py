a3f2acfdb6bd79f13889774fece4b7772f663612
Drill <sdi6566@gmail.com>
2025-07-26 11:13:04 +0900

    ✨ Feat: ChronicDisease, Allergy, Medication models and M2M relations to User
    건강 정보를 M2M 관계로 유저와 연결

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,36 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 import uuid
+
+
+class HealthInfoBase(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField("이름", max_length=100, unique=True)
+    description = models.TextField("설명", blank=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class ChronicDisease(HealthInfoBase):
+    class Meta:
+        verbose_name = "지병"
+        verbose_name_plural = "지병 목록"
+
+
+class Allergy(HealthInfoBase):
+    class Meta:
+        verbose_name = "알레르기"
+        verbose_name_plural = "알레르기 목록"
+
+
+class Medication(HealthInfoBase):
+    class Meta:
+        verbose_name = "복용 약물"
+        verbose_name_plural = "복용 약물 목록"
 
 
 class User(AbstractUser):
@@ -16,7 +47,13 @@ class User(AbstractUser):
         blank=True,
     )
     health_goals = models.CharField("건강 목표", max_length=255, blank=True)
-
+    chronic_diseases = models.ManyToManyField(
+        ChronicDisease, blank=True, verbose_name="지병"
+    )
+    allergies = models.ManyToManyField(Allergy, blank=True, verbose_name="알레르기")
+    medications = models.ManyToManyField(
+        Medication, blank=True, verbose_name="복용 약물"
+    )
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["nickname"]
 
