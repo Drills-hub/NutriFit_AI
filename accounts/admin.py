31289096fcbc2d53204e87289d95a42bb0f8b389
Drill <sdi6566@gmail.com>
2025-07-30 10:50:20 +0900

    ✨ Feat: add admin page accounts, allergy, chronicdisease, medication
    관리자 페이지에 회원, 알러지, 지병, 약물 추가

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,67 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from .models import Allergy, ChronicDisease, Medication, User
+
+
+# 지병, 알레르기, 복용 약물 모델을 위한 기본 Admin 클래스
+class HealthInfoBaseAdmin(admin.ModelAdmin):
+    list_display = ("name", "description")
+    search_fields = ("name",)
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("개인 정보", {"fields": ("nickname", "birth_date", "gender")}),
+        (
+            "건강 정보",
+            {
+                "fields": (
+                    "health_goals",
+                    "chronic_diseases",
+                    "allergies",
+                    "medications",
+                )
+            },
+        ),
+        (
+            "권한",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("주요 날짜", {"fields": ("last_login", "date_joined")}),
+    )
+    list_display = ("email", "nickname", "is_staff")
+    search_fields = ("email", "nickname")
+    ordering = ("email",)
+    filter_horizontal = (
+        "groups",
+        "user_permissions",
+        "chronic_diseases",
+        "allergies",
+        "medications",
+    )
+
+
+@admin.register(ChronicDisease)
+class ChronicDiseaseAdmin(HealthInfoBaseAdmin):
+    pass
+
+
+@admin.register(Allergy)
+class AllergyAdmin(HealthInfoBaseAdmin):
+    pass
+
+
+@admin.register(Medication)
+class MedicationAdmin(HealthInfoBaseAdmin):
+    pass
