de294a4571b2585860d39f67d6e42ef204597d71
Drill <sdi6566@gmail.com>
2025-08-22 21:00:27 +0900

    ✨ Feat: swagger-ui
    스웨거를 이용한 api 명세서 구현

--- backend/urls.py
+++ backend/urls.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/accounts/", include("accounts.urls")),
     path("api/v1/chats/", include("chats.urls")),
+    # drf-spectacular
+    path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/v1/schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
 ]
